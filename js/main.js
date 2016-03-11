2178f7e426775154c77fb5907504b4b6c617797b
Evan Lefebvre <lefe0177@algonquinlive.com>
2016-03-10 21:39:25 -0500

    Adjusted spacing

--- js/main.js
+++ js/main.js
@@ -33,12 +33,10 @@ var redCurrant = {
 // END DO NOT CHANGE ZONE
 // ##########################################################
 
-
 // Write your function here, name it `writeBerries`
 var writeBerries = function (berries) {
   document.write('<h1>Berries</h1>');
   document.write('<ol>');
-
 
   berries.forEach(function (berry) {
     document.write('<h2>' + berry.name + '</h2>');
